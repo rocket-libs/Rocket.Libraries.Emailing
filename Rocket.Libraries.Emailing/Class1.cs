1bd7e03aa4c5d0358f59922306028de01151f870
Nyingi Maina <nyingimaina@gmail.com>
2018-03-11 21:24:48 +0300

    Reworked layout

--- Rocket.Libraries.Emailing/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Rocket.Libraries.Emailing
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Rocket.Libraries.Emailing/Emailer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rocket.Libraries.Emailing
+{
+    class Emailer
+    {
+    }
+}
