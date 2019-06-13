58840f595bb058f5e8672c7a831cdfc26287c468
Nyingi Maina <nyingimaina@gmail.com>
2019-06-13 13:26:55 +0300

    Created a factory to resolve the correct type of receiver

---
+++ src/Rocket.Libraries.Emailing/Services/Receiving/MailReceiverFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace Rocket.Libraries.Emailing.Services.Receiving
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using MailKit.Net.Imap;
+    using MailKit.Net.Pop3;
+    using Rocket.Libraries.Emailing.Services.Receiving.Imap;
+    using Rocket.Libraries.Emailing.Services.Receiving.Pop3;
+    using Rocket.Libraries.Validation.Services;
+
+    public static class MailReceiverFactory
+    {
+        public static MailReceiverBase<TClient> Create<TClient>()
+        {
+            if (typeof(TClient) == typeof(ImapClient))
+            {
+                return new ImapReceiver() as MailReceiverBase<TClient>;
+            }
+            else if (typeof(TClient) == typeof(Pop3Client))
+            {
+                return new Pop3Receiver() as MailReceiverBase<TClient>;
+            }
+            else
+            {
+                new DataValidator().EvaluateImmediate(() => true, $"Unsupported client type '{typeof(TClient).Name}'. Cannot create a receiver");
+                return null;
+            }
+        }
+    }
+}
